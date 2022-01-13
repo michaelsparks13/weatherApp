23f13111cc10e5ec09474e77a184d28a87961ca1
Michael Sparks <michael.sparks13@gmail.com>
2022-01-13 08:24:08 -0500

    set up views & partials paths

--- src/app.js
+++ src/app.js
@@ -7,10 +7,14 @@ const port = process.env.PORT || 3000
 
 const publicStaticDirPath = path.join(__dirname, '../public')
 
-const viewPath = path.join(__dirname, '../templates/views');
+const viewsPath = path.join(__dirname, '../templates/views');
 
 const partialsPath = path.join(__dirname, '../templates/partials')
 
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath);
 app.use(express.static(publicStaticDirPath));
 
 app.get('', (req, res) => {
