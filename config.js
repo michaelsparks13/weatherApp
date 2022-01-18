4de761112afa1bf5f8b7a3d6f73b61fb44144eb3
Michael Sparks <michael.sparks13@gmail.com>
2022-01-18 10:15:04 -0500

    fixing api key

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "61ab58a0abf464f38c103aad7c609cbc",
+        SECRET_KEY: YOUR_API_KEY,
     }
 }
 
